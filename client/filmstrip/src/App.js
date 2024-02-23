5febbd9e33a775e126ed49625bff56d3eaeaad61
Lukasz <lukasz.wilk231@gmail.com>
2024-02-23 15:27:32 +0100

    working routing for login and register pages

--- client/filmstrip/src/App.js
+++ client/filmstrip/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import PrivateRoute from './PirvateRoute';
 import Login from './Login';
 import Home from './Home';
+import Register from './Register';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route element={<PrivateRoute/>}>
           <Route path="/" element={<Home/>}/>
         </Route>
+        <Route path="/register" element={<Register/>}/> 
         <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
--- client/filmstrip/src/Register.jsx
+++ client/filmstrip/src/Register.jsx
@@ -1,18 +1,18 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-const Login = prop => {
+const Register = prop => {
 
     const navigate = useNavigate();
 
-    const handleRegisterClick = () => {
-        navigate("/register");
+    const handleBackClick = () => {
+        navigate("/login");
     }
 
     return (
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
     <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"></img>
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight color-yellow">FILMSTRIP</h2>
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight color-yellow">Register</h2>
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
@@ -34,10 +34,19 @@ const Login = prop => {
       </div>
 
       <div>
-        <button id="loginButton" type="submit" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 color-yellow">Repeat Password</label>
+        </div>
+        <div class="mt-2">
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+        </div>
+      </div>
+
+      <div>
+        <button id="registerButton" type="submit" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
       </div>
       <div>
-        <button id="registerButton" type="button" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleRegisterClick}>Register</button>
+        <button id="backButton" type="button" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleBackClick}>Back</button>
       </div>
     </form>
   </div>
@@ -46,4 +55,4 @@ const Login = prop => {
     )
 }
 
-export default Login
+export default Register;
