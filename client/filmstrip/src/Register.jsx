74f2b774ebfbfe36a855a0e1d5296632a3cc01a3
Lukasz <lukasz.wilk231@gmail.com>
2024-02-24 13:25:06 +0100

    added complex state to handle changes in register component

--- client/filmstrip/src/Register.jsx
+++ client/filmstrip/src/Register.jsx
@@ -1,26 +1,43 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Register = prop => {
 
     const navigate = useNavigate();
 
+    const [regiserData, setRegisterData] = useState({
+      login: '',
+      password: '',
+      repeatedPassword: '',
+    });
+
     const handleBackClick = () => {
         navigate("/login");
     }
 
+    const handleInputChange = e => {
+
+      const {name, value} = e.target;
+
+      setRegisterData(prevVal => {
+        return {
+          ...prevVal,
+          [name]: value,
+        };
+      });
+    };
+
     return (
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
-    <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"></img>
+  <div id="register" class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm rounded-lg space-y-6 p-2">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
+      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"></img>
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight color-yellow">Register</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
-    <form class="space-y-6" action="#" method="POST">
+    </div>
       <div>
         <label for="email" class="block text-sm font-medium leading-6 color-yellow">Login</label>
         <div class="mt-2">
-          <input id="login" name="login" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+          <input id="login" name="login" value={regiserData.login} onChange={handleInputChange} required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
         </div>
       </div>
 
@@ -29,7 +46,7 @@ const Register = prop => {
           <label for="password" class="block text-sm font-medium leading-6 color-yellow">Password</label>
         </div>
         <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+          <input id="password" name="password" type="password" value={regiserData.password} onChange={handleInputChange} autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
         </div>
       </div>
 
@@ -38,7 +55,7 @@ const Register = prop => {
           <label for="password" class="block text-sm font-medium leading-6 color-yellow">Repeat Password</label>
         </div>
         <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+          <input id="repeatedPassword" name="repeatedPassword" type="password" value={regiserData.repeatedPassword} onChange={handleInputChange} autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
         </div>
       </div>
 
@@ -48,7 +65,6 @@ const Register = prop => {
       <div>
         <button id="backButton" type="button" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleBackClick}>Back</button>
       </div>
-    </form>
   </div>
 </div>
 
