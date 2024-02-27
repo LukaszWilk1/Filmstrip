198d7e4804720569b0fff709a0a070f754c96fce
Lukasz <lukasz.wilk231@gmail.com>
2024-02-27 11:49:14 +0100

    added hadnling login inputs state

--- client/filmstrip/src/Login.jsx
+++ client/filmstrip/src/Login.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 const Login = prop => {
 
+    const [loginData, setLoginData] = useState({
+      login: '',
+      password: '',
+    })
+
     const navigate = useNavigate();
+
+    const handleInputChange = e => {
+
+      const {name, value} = e.target;
+
+      setLoginData(prevVal => {
+        return {
+          ...prevVal,
+          [name]: value,
+        };
+      });
+    }
 
     const handleRegisterClick = () => {
         navigate("/register");
@@ -19,7 +37,7 @@ const Login = prop => {
       <div>
         <label for="email" class="block text-sm font-medium leading-6 color-yellow">Login</label>
         <div class="mt-2">
-          <input id="loginInput" name="login" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+          <input id="loginInput" name="login" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" onChange={handleInputChange} value={loginData.login}></input>
         </div>
       </div>
 
@@ -28,7 +46,7 @@ const Login = prop => {
           <label for="password" class="block text-sm font-medium leading-6 color-yellow">Password</label>
         </div>
         <div class="mt-2">
-          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2" onChange={handleInputChange} value={loginData.password}></input>
         </div>
       </div>
 
