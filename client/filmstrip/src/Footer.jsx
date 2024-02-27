3eca5bb0dcd7e47852bde3937a4f6d63c4f4a950
Lukasz <lukasz.wilk231@gmail.com>
2024-02-27 14:39:18 +0100

    added footer

---
+++ client/filmstrip/src/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <div id="footer" className="w-full py-7 text-center relative bottom-0 left-0">
+            @Łukasz Wilk | <a href="https://www.themoviedb.org/?language=pl-PL">TMDB</a>
+        </div>
+    )
+}
--- client/filmstrip/src/Home.jsx
+++ client/filmstrip/src/Home.jsx
@@ -1,12 +1,14 @@
 import { useAuth } from "./auth";
+import { Footer } from "./Footer";
 
 const Home = () => {
 
     const auth = useAuth();
 
     return (
-        <div>
+        <div id="home" className="w-full h-full">
             <button onClick={auth.logout}>Logout</button>
+            <Footer/>
         </div>
     )
 }
