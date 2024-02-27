27860972d7671e50b75c917e50f5e1c8adab097b
Lukasz <lukasz.wilk231@gmail.com>
2024-02-27 11:43:06 +0100

    fixed login style

--- client/filmstrip/src/Login.jsx
+++ client/filmstrip/src/Login.jsx
@@ -10,17 +10,16 @@ const Login = prop => {
 
     return (
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm ">
+  <div id="login" class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm border space-y-6 p-2 rounded-lg">
+
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"></img>
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight color-yellow">FILMSTRIP</h2>
   </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
-    <form class="space-y-6" action="#" method="POST">
       <div>
         <label for="email" class="block text-sm font-medium leading-6 color-yellow">Login</label>
         <div class="mt-2">
-          <input id="login" name="login" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
+          <input id="loginInput" name="login" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"></input>
         </div>
       </div>
 
@@ -34,12 +33,11 @@ const Login = prop => {
       </div>
 
       <div>
-        <button id="loginButton" type="submit" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
+        <button id="loginButton" type="button" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
       </div>
       <div>
         <button id="registerButton" type="button" class="flex w-full justify-center rounded-md button-color px-3 py-1.5 text-sm font-semibold leading-6 color-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleRegisterClick}>Register</button>
       </div>
-    </form>
   </div>
 </div>
 
