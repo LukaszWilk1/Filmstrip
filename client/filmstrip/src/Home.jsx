27a0117d1ab7806a6d3ae17854618b9236bf835b
Lukasz <lukasz.wilk231@gmail.com>
2024-02-28 15:30:32 +0100

    working api call with fetch and added .env file for client

--- client/filmstrip/src/Home.jsx
+++ client/filmstrip/src/Home.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "./auth";
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjJlMmQ3MWM0MDMyMTMzMDk0YWE4MTNhNzdhZjFhMyIsInN1YiI6IjY1ZDM1MDZlZjQ5NWVlMDE3YzQwNWYwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3THJaqU3vl3xsAm4m7VaKGyULMNbsZKrbQzK8z82Hlc'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
 const Home = () => {
 
     const auth = useAuth();
+
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+    useEffect(() => {
+        const trendingMoviesList = [];
+        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+    .   catch(err => console.error(err));
+    }, []);
 
     return (
         <div id="home" className="w-full h-full">
