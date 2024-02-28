65b7a45e23429e35a37677cb0dad764a4f72af66
Lukasz <lukasz.wilk231@gmail.com>
2024-02-28 15:48:06 +0100

    maping movie list to divs

--- client/filmstrip/src/Home.jsx
+++ client/filmstrip/src/Home.jsx
@@ -10,11 +10,6 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjJlMmQ3MWM0MDMyMTMzMDk0YWE4MTNhNzdhZjFhMyIsInN1YiI6IjY1ZDM1MDZlZjQ5NWVlMDE3YzQwNWYwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3THJaqU3vl3xsAm4m7VaKGyULMNbsZKrbQzK8z82Hlc'
     }
   };
-  
-  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
 
 const Home = () => {
 
@@ -23,17 +18,20 @@ const Home = () => {
     const [trendingMovies, setTrendingMovies] = useState([]);
 
     useEffect(() => {
-        const trendingMoviesList = [];
+        let moviesList = [];
         fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
         .then(response => response.json())
-        .then(response => console.log(response))
-    .   catch(err => console.error(err));
+        .then(response => {
+            setTrendingMovies(response.results);
+        })
+        .catch(err => console.error(err));
     }, []);
 
     return (
         <div id="home" className="w-full h-full">
             <Navbar/>
                 <h1 className="text-[#ffd500] text-center text-[4rem]">TRENDING MOVIES</h1>
+                {trendingMovies.map(movie => (<div key={movie.id} id={movie.id}></div>))}
             <Footer/>
         </div>
     )
