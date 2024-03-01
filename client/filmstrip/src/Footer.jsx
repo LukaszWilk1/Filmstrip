afee9858ce200ffafd0fb04b97ceaca793535cbe
Lukasz <lukasz.wilk231@gmail.com>
2024-03-01 17:46:19 +0100

    finished styling movie component

--- client/filmstrip/src/Footer.jsx
+++ client/filmstrip/src/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <div id="footer" className="w-full py-7 text-center">
+        <div id="footer" className="py-7 text-center">
             @Łukasz Wilk | <a href="https://www.themoviedb.org/?language=pl-PL">TMDB</a>
         </div>
     )
