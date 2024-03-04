4cdf63f5b6cac202fde176ecaf815074366292c6
Lukasz <lukasz.wilk231@gmail.com>
2024-03-04 12:49:20 +0100

    working searching for movies and series

--- client/filmstrip/src/SeriesPanel.jsx
+++ client/filmstrip/src/SeriesPanel.jsx
@@ -6,9 +6,9 @@ const SeriesPanel = ({ series, index }) => {
     return (
         <Link key={index} to={"/series/" + series.id}>
             <div id={series.id} className="relative mb-2 sm:mb-0">
-                <div className="absolute top-0 left-0 bg-black bg-opacity-50 text-white rounded-full px-2 py-1 text-xs">
+                {index ? <div className="absolute top-0 left-0 bg-black bg-opacity-50 text-white rounded-full px-2 py-1 text-xs">
                     {index + 1}
-                </div>
+                </div> : <></>}
                     <img src={imgSrc + series.poster_path} alt={series.title} className="w-full" />
             </div>
         </Link>
