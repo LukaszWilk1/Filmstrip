7080ed6488e79bdd62b281162a84e7496325b2a0
Lukasz <lukasz.wilk231@gmail.com>
2024-03-05 20:03:57 +0100

    removed middleware

--- server/index.js
+++ server/index.js
@@ -36,14 +36,6 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-  
-
 app.use(cors());
 
 app.use(bodyParser.json());
