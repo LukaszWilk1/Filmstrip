5fb8f15304a39f19f0fb186f235ea2748603bd03
Lukasz <lukasz.wilk231@gmail.com>
2024-03-05 20:16:22 +0100

    test

--- server/index.js
+++ server/index.js
@@ -99,7 +99,6 @@ app.post("/register", async (req, res) => {
 
 app.get("/", async(req,res) => {
     await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-        .then(response => response.json())
         .then(response => {
             res.send(response);
         })
@@ -121,7 +120,6 @@ app.get("/series", (req, res) => {
     fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', options)
         .then(response => response.json())
         .then(response => {
-            console.log(response);
             res.send(response);
         })
         .catch(err => {
