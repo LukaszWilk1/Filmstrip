9e6023365dac34b2a6e6d37eafe913fb74e2fd4d
Lukasz <lukasz.wilk231@gmail.com>
2024-03-05 20:43:50 +0100

    test

--- server/index.js
+++ server/index.js
@@ -42,6 +42,10 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../client/filmstrip/build')));
 
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+
 app.post("/login", async (req, res) => {
     await db.query("Select user_password, id FROM users WHERE login LIKE $1", [req.body.login], (err, resDb) => {
         if(err){
